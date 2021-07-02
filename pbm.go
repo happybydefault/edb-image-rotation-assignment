f831dfd0ba565982bdc2077ad5778eec05c74682
Raúl Garrido <me@happybydefault.com>
2021-07-02 18:16:49 -0400

    Modify Flip function

--- pbm.go
+++ pbm.go
@@ -2,15 +2,19 @@
 package pbm
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"io"
+	"log"
+	"strconv"
+	"strings"
 )
 
 // Flip returns the rotated image of an ASCII encoded PBM. The degrees should be a multiple of a quarter turn (e.g. 90, 180, -270, etc.), otherwise it
 // returns a non-nil error.
-func Flip(r io.Reader, degrees int, ccw bool) ([]byte, error) {
-	if r == nil {
+func Flip(image io.Reader, degrees int, ccw bool) ([]byte, error) {
+	if image == nil {
 		return nil, errors.New("image is nil")
 	}
 
@@ -19,10 +23,30 @@ func Flip(r io.Reader, degrees int, ccw bool) ([]byte, error) {
 		return nil, fmt.Errorf("number of degrees is not multiple of a quarter turn")
 	}
 
-	b, err := io.ReadAll(r)
+	r := bufio.NewReader(image)
+
+	sizeStr, err := r.ReadString('\n')
 	if err != nil {
-		return nil, fmt.Errorf("could not read image: %w", err)
+		return nil, err
+	}
+	sizeStr = strings.TrimSuffix(sizeStr, "\n")
+
+	size := strings.Split(sizeStr, " ")
+	if len(size) < 2 {
+		return nil, fmt.Errorf("invalid size string")
 	}
 
-	return b, nil
+	width, err := strconv.Atoi(size[0])
+	if err != nil {
+		return nil, fmt.Errorf("invalid width: %w", err)
+	}
+
+	height, err := strconv.Atoi(size[1])
+	if err != nil {
+		return nil, fmt.Errorf("invalid height: %w", err)
+	}
+
+	log.Printf("width: %d, height: %d", width, height)
+
+	return nil, nil
 }
