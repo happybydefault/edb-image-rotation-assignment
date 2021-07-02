90e53928c297eb402a2cf4edafdbce3caa2e5ecb
Raúl Garrido <me@happybydefault.com>
2021-07-02 15:38:11 -0400

    Add Flip function

--- pbm.go
+++ pbm.go
@@ -1,1 +1,23 @@
+// Package pbm is a library for manipulating PBM images.
 package pbm
+
+import (
+	"errors"
+	"fmt"
+	"io"
+)
+
+// Flip rotates an image with a degrees that's a multiple of a quarter turn (e.g. 90, 180, -270, etc.), otherwise it
+// returns a non-nil error.
+func Flip(r io.Reader, degrees int) (io.Reader, error) {
+	if r == nil {
+		return nil, errors.New("reader is nil")
+	}
+
+	quarterTurn := 90
+	if degrees%quarterTurn > 0 {
+		return nil, fmt.Errorf("number of degrees is not multiple of a quarter turn")
+	}
+
+	return r, nil
+}
--- pbm_test.go
+++ pbm_test.go
@@ -1,1 +1,70 @@
 package pbm
+
+import (
+	"bytes"
+	"io"
+	"reflect"
+	"testing"
+)
+
+func TestFlip(t *testing.T) {
+	image := `P1
+# This is an example bitmap of the letter "J"
+6 10
+0 0 0 0 1 0
+0 0 0 0 1 0
+0 0 0 0 1 0
+0 0 0 0 1 0
+0 0 0 0 1 0
+0 0 0 0 1 0
+1 0 0 0 1 0
+0 1 1 1 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+`
+	imageCW90 := `P1
+# This is an example bitmap of the letter "J"
+10 6
+0 0 0 1 0 0 0 0 0 0
+0 0 1 0 0 0 0 0 0 0
+0 0 1 0 0 0 0 0 0 0
+0 0 1 0 0 0 0 0 0 0
+0 0 0 1 1 1 1 1 1 1
+0 0 0 0 0 0 0 0 0 0
+`
+
+	type args struct {
+		r       io.Reader
+		degrees int
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    io.Reader
+		wantErr bool
+	}{
+		{
+			name: "assignment example, 90 degrees clockwise",
+			args: args{
+				r:       bytes.NewReader([]byte(image)),
+				degrees: 90,
+			},
+			want:    bytes.NewReader([]byte(imageCW90)),
+			wantErr: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := Flip(test.args.r, test.args.degrees)
+			if (err != nil) != test.wantErr {
+				t.Errorf("Flip() error = %v, wantErr %v", err, test.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, test.want) {
+				t.Errorf("Flip() got = %v, want %v", got, test.want)
+			}
+		})
+	}
+}
