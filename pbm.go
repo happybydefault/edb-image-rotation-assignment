71bdf83b27aec4b94a2b71300539730bc3b35f99
Raúl Garrido <me@happybydefault.com>
2021-07-06 18:45:30 -0400

    Add FlipOptimized function and modify Flip function

--- pbm.go
+++ pbm.go
@@ -7,7 +7,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"log"
 	"strconv"
 	"strings"
 )
@@ -71,9 +70,6 @@ func Flip(output io.Writer, image io.Reader, degrees int, ccw bool) error {
 	if err != nil {
 		return fmt.Errorf("invalid height: %w", err)
 	}
-	log.Printf("width: %d, height: %d", originalWidth, originalHeight)
-
-	// TODO
 
 	_, err = fmt.Fprintln(output, string(magicNumASCII))
 	if err != nil {
@@ -81,11 +77,6 @@ func Flip(output io.Writer, image io.Reader, degrees int, ccw bool) error {
 	}
 
 	_, err = fmt.Fprint(output, comments)
-	if err != nil {
-		return fmt.Errorf("could not write to output: %w", err)
-	}
-
-	_, err = fmt.Fprintln(output, "# Flipped")
 	if err != nil {
 		return fmt.Errorf("could not write to output: %w", err)
 	}
@@ -120,14 +111,131 @@ func Flip(output io.Writer, image io.Reader, degrees int, ccw bool) error {
 			continue
 		}
 
-		// TODO
-		originalX := count % originalWidth
-		originalY := count / originalWidth
-		log.Println(originalX, originalY)
+		x := (width - 1) - (count / height)
+		y := count % height
+
+		matrix[y][x] = b
 
 		count++
 	}
-	log.Printf("%+v", matrix)
+
+	// TODO
+	for _, y := range matrix {
+		for _, x := range y {
+			_, err := fmt.Fprint(output, string(x)+" ")
+			if err != nil {
+				return fmt.Errorf("could not write to output: %w", err)
+			}
+		}
+		fmt.Fprint(output, "\n")
+	}
+
+	return nil
+}
+
+// FlipOptimized writes the rotated image of an ASCII encoded PBM to out. The degrees should be a multiple of a quarter turn (e.g. 90, 180, -270, etc.), otherwise it
+// returns a non-nil error.
+func FlipOptimized(output io.Writer, image io.Reader, degrees int, ccw bool) error {
+	if image == nil {
+		return errors.New("image is nil")
+	}
+
+	quarterTurn := 90
+	if degrees%quarterTurn > 0 {
+		return errors.New("number of degrees is not multiple of a quarter turn")
+	}
+
+	r := bufio.NewReader(image)
+	magicNum := make([]byte, 2)
+	_, err := r.Read(magicNum)
+	if err != nil {
+		return fmt.Errorf("could not read magic number: %w", err)
+	}
+	if !bytes.Equal(magicNum, magicNumASCII) {
+		return fmt.Errorf("magic number does not correspond to an ASCII PBM file: %w", err)
+	}
+
+	var sizeStr string
+	for {
+		s, err := r.ReadString('\n')
+		if err != nil {
+			return fmt.Errorf("could not read string: %w", err)
+		}
+		s = strings.TrimSpace(s)
+
+		if s == "" || strings.HasPrefix(s, "#") {
+			continue
+		} else {
+			sizeStr = s
+			break
+		}
+	}
+
+	size := strings.Split(sizeStr, " ")
+	if len(size) < 2 {
+		return errors.New("invalid size string")
+	}
+
+	originalWidth, err := strconv.Atoi(size[0])
+	if err != nil {
+		return fmt.Errorf("invalid width: %w", err)
+	}
+	originalHeight, err := strconv.Atoi(size[1])
+	if err != nil {
+		return fmt.Errorf("invalid height: %w", err)
+	}
+
+	_, err = fmt.Fprintln(output, string(magicNumASCII))
+	if err != nil {
+		return fmt.Errorf("could not write to output: %w", err)
+	}
+
+	_, err = fmt.Fprintln(output, originalHeight, originalWidth)
+	if err != nil {
+		return fmt.Errorf("could not write to output: %w", err)
+	}
+
+	width, height := originalHeight, originalWidth
+	matrix := make([][]byte, height)
+	for i := range matrix {
+		matrix[i] = make([]byte, width)
+	}
+
+	var count int
+	for {
+		b, err := r.ReadByte()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return fmt.Errorf("could not read byte: %w", err)
+		}
+
+		space := false
+		switch b {
+		case ' ', '\t', '\n', '\r':
+			space = true
+		}
+		if space {
+			continue
+		}
+
+		x := (width - 1) - (count / height)
+		y := count % height
+
+		matrix[y][x] = b
+
+		count++
+	}
+
+	for _, y := range matrix {
+		for _, x := range y {
+			_, err := fmt.Fprint(output, string(x))
+			if err != nil {
+				return fmt.Errorf("could not write to output: %w", err)
+			}
+		}
+	}
 
 	return nil
 }
