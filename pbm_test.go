70c642848d9aac5ed0e6302e5487a625ef090681
Raúl Garrido <me@happybydefault.com>
2021-07-08 04:50:57 -0400

    Modify tests

--- pbm_test.go
+++ pbm_test.go
@@ -4,11 +4,13 @@ import (
 	"bytes"
 	"io"
 	"reflect"
+	"strings"
 	"testing"
 )
 
-func TestFlip(t *testing.T) {
-	image := []byte(`6 10
+func TestRotate(t *testing.T) {
+	image := `P1
+6 10
 0 0 0 0 1 0
 0 0 0 0 1 0
 0 0 0 0 1 0
@@ -19,15 +21,69 @@ func TestFlip(t *testing.T) {
 0 1 1 1 0 0
 0 0 0 0 0 0
 0 0 0 0 0 0
-`)
-	imageCW90 := []byte(`10 6
+`
+
+	image90 := `P1
+10 6
 0 0 0 1 0 0 0 0 0 0
 0 0 1 0 0 0 0 0 0 0
 0 0 1 0 0 0 0 0 0 0
 0 0 1 0 0 0 0 0 0 0
 0 0 0 1 1 1 1 1 1 1
 0 0 0 0 0 0 0 0 0 0
-`)
+`
+
+	image180 := `P1
+6 10
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 1 1 1 0
+0 1 0 0 0 1
+0 1 0 0 0 0
+0 1 0 0 0 0
+0 1 0 0 0 0
+0 1 0 0 0 0
+0 1 0 0 0 0
+0 1 0 0 0 0
+`
+
+	image270 := `P1
+10 6
+0 0 0 0 0 0 0 0 0 0
+1 1 1 1 1 1 1 0 0 0
+0 0 0 0 0 0 0 1 0 0
+0 0 0 0 0 0 0 1 0 0
+0 0 0 0 0 0 0 1 0 0
+0 0 0 0 0 0 1 0 0 0
+`
+
+	imageWithComments := `P1
+# Some
+# comments
+6 10
+0 0 0 0 1 0
+0 0 0 0 1 0
+0 0 0 0 1 0
+0 0 0 0 1 0
+0 0 0 0 1 0
+0 0 0 0 1 0
+1 0 0 0 1 0
+0 1 1 1 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+`
+
+	imageWithoutWhitespaces := `P1
+# Some
+# comments
+6 10
+000010000010000010000010000010000010100010011100000000000000
+`
+
+	imageInvalidSize := `P1
+50 -1
+000010000010000010000010000010000010100010011100000000000000
+`
 
 	type args struct {
 		image   io.Reader
@@ -38,30 +94,92 @@ func TestFlip(t *testing.T) {
 	tests := []struct {
 		name    string
 		args    args
-		want    []byte
+		want    string
 		wantErr bool
 	}{
 		{
 			name: "assignment example, 90 degrees clockwise",
 			args: args{
-				image:   bytes.NewReader(image),
+				image:   strings.NewReader(image),
 				degrees: 90,
 				ccw:     false,
 			},
-			want:    imageCW90,
+			want:    image90,
 			wantErr: false,
+		},
+		{
+			name: "assignment example, 180 degrees clockwise",
+			args: args{
+				image:   strings.NewReader(image),
+				degrees: 180,
+				ccw:     false,
+			},
+			want:    image180,
+			wantErr: false,
+		},
+		{
+			name: "assignment example, 270 degrees clockwise",
+			args: args{
+				image:   strings.NewReader(image),
+				degrees: 270,
+				ccw:     false,
+			},
+			want:    image270,
+			wantErr: false,
+		},
+		{
+			name: "assignment example, 270 degrees counterclockwise",
+			args: args{
+				image:   strings.NewReader(image),
+				degrees: 270,
+				ccw:     true,
+			},
+			want:    image90,
+			wantErr: false,
+		},
+		{
+			name: "assignment example with comments, -90 degrees counterclockwise",
+			args: args{
+				image:   strings.NewReader(imageWithComments),
+				degrees: -90,
+				ccw:     true,
+			},
+			want:    image90,
+			wantErr: false,
+		},
+		{
+			name: "assignment example with comments and without whitespaces, -270 degrees clockwise",
+			args: args{
+				image:   strings.NewReader(imageWithoutWhitespaces),
+				degrees: -270,
+				ccw:     false,
+			},
+			want:    image90,
+			wantErr: false,
+		},
+		{
+			name: "assignment example with invalid size, 90 degrees clockwise",
+			args: args{
+				image:   strings.NewReader(imageInvalidSize),
+				degrees: 90,
+				ccw:     false,
+			},
+			want:    "",
+			wantErr: true,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			got, err := Flip(test.args.image, test.args.degrees, test.args.ccw)
+			buf := &bytes.Buffer{}
+			err := Rotate(buf, test.args.image, test.args.degrees, test.args.ccw)
 			if (err != nil) != test.wantErr {
-				t.Errorf("Flip() error = %v, wantErr %v", err, test.wantErr)
+				t.Errorf("Rotate() error = %v, wantErr %v", err, test.wantErr)
 				return
 			}
+			got := buf.String()
 			if !reflect.DeepEqual(got, test.want) {
-				t.Errorf("Flip() got = %v, want %v", got, test.want)
+				t.Errorf("Rotate() got = %v, want %v", got, test.want)
 			}
 		})
 	}
