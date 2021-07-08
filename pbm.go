c1af98f227eabcd842fa2a6eb0645022e8e5fa4d
Raúl Garrido <me@happybydefault.com>
2021-07-08 04:05:52 -0400

    Modify Rotate function

--- pbm.go
+++ pbm.go
@@ -7,9 +7,9 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"log"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 var magicNumASCII = []byte("P1")
@@ -29,8 +29,19 @@ func Rotate(output io.Writer, image io.Reader, degrees int, ccw bool) error {
 		return errors.New("number of degrees should be a multiple of a quarter turn")
 	}
 
-	rotations := degrees / 90 % 4
-	log.Printf("rotations: %v", rotations)
+	if ccw {
+		degrees *= -1
+	}
+
+	rotations := (degrees/90%4 + 4) % 4
+
+	if rotations == 0 {
+		_, err := io.Copy(output, image)
+		if err != nil {
+			return fmt.Errorf("could not copy: %w", err)
+		}
+		return nil
+	}
 
 	r := bufio.NewReader(image)
 	magicNum := make([]byte, 2)
@@ -42,66 +53,125 @@ func Rotate(output io.Writer, image io.Reader, degrees int, ccw bool) error {
 		return errors.New("magic number does not correspond to an ASCII encoded, PBM file")
 	}
 
-	var (
-		sizeStr  string
-		comments = &bytes.Buffer{}
-	)
+	var width, height int
+loop:
 	for {
-		s, err := r.ReadString('\n')
+		b, err := r.ReadByte()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return fmt.Errorf("could not read byte: %w", err)
+		}
+
+		if unicode.IsSpace(rune(b)) {
+			continue
+		} else if b == '#' {
+			_, err := r.ReadBytes('\n')
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break loop
+				}
+				return fmt.Errorf("could not read bytes: %w", err)
+			}
+
+			continue
+		}
+
+		err = r.UnreadByte()
+		if err != nil {
+			return fmt.Errorf("could not unread byte: %w", err)
+		}
+
+		sizeStr, err := r.ReadString('\n')
 		if err != nil {
 			return fmt.Errorf("could not read string: %w", err)
 		}
-		s = strings.TrimSpace(s)
+		sizeStr = strings.TrimSpace(sizeStr)
 
-		if s == "" {
-			continue
-		} else if strings.HasPrefix(s, "#") {
-			fmt.Fprintln(comments, s)
-			continue
-		} else {
-			sizeStr = s
-			break
+		size := strings.Split(sizeStr, " ")
+		if len(size) < 2 {
+			return errors.New("invalid width or height")
+		}
+		widthStr := size[0]
+		heightStr := size[1]
+
+		widthN, err := strconv.Atoi(widthStr)
+		if err != nil {
+			return fmt.Errorf("invalid width: %w", err)
+		}
+		width = widthN
+
+		heightN, err := strconv.Atoi(heightStr)
+		if err != nil {
+			return fmt.Errorf("invalid height: %w", err)
+		}
+		height = heightN
+
+		break
+	}
+
+	w := bufio.NewWriter(output)
+	defer w.Flush()
+
+	_, err = fmt.Fprintln(w, string(magicNumASCII))
+	if err != nil {
+		return fmt.Errorf("could not write: %w", err)
+	}
+
+	switch rotations {
+	case 1:
+		_, err = fmt.Fprintln(w, height, width)
+		if err != nil {
+			return fmt.Errorf("could not write: %w", err)
+		}
+
+		err = rotate90(r, w, width, height)
+		if err != nil {
+			return fmt.Errorf("could not rotate 90 degrees: %w", err)
+		}
+	case 2:
+		_, err = fmt.Fprintln(w, width, height)
+		if err != nil {
+			return fmt.Errorf("could not write: %w", err)
+		}
+
+		err = rotate180(r, w, width, height)
+		if err != nil {
+			return fmt.Errorf("could not rotate 180 degrees: %w", err)
+		}
+	case 3:
+		_, err = fmt.Fprintln(w, height, width)
+		if err != nil {
+			return fmt.Errorf("could not write: %w", err)
+		}
+
+		err = rotate270(r, w, width, height)
+		if err != nil {
+			return fmt.Errorf("could not rotate 270 degrees: %w", err)
 		}
 	}
 
-	size := strings.Split(sizeStr, " ")
-	if len(size) < 2 {
-		return errors.New("invalid size string")
+	_, err = fmt.Fprint(w, "\n")
+	if err != nil {
+		return fmt.Errorf("could not write: %w", err)
 	}
 
-	originalWidth, err := strconv.Atoi(size[0])
-	if err != nil {
-		return fmt.Errorf("invalid width: %w", err)
-	}
-	originalHeight, err := strconv.Atoi(size[1])
-	if err != nil {
-		return fmt.Errorf("invalid height: %w", err)
-	}
+	return nil
+}
 
-	_, err = fmt.Fprintln(output, string(magicNumASCII))
-	if err != nil {
-		return fmt.Errorf("could not write to output: %w", err)
-	}
+func rotate90(r io.Reader, w io.Writer, width, height int) error {
+	width, height = height, width
 
-	_, err = fmt.Fprint(output, comments)
-	if err != nil {
-		return fmt.Errorf("could not write to output: %w", err)
-	}
-
-	_, err = fmt.Fprintln(output, originalHeight, originalWidth)
-	if err != nil {
-		return fmt.Errorf("could not write to output: %w", err)
-	}
-
-	width, height := originalHeight, originalWidth
-	matrix := make([][]byte, height)
+	matrix := make([][]string, height)
 	for i := range matrix {
-		matrix[i] = make([]byte, width)
+		matrix[i] = make([]string, width)
 	}
 
+	rd := bufio.NewReader(r)
 	var count int
 	for {
-		b, err := r.ReadByte()
+		b, err := rd.ReadByte()
 		if err != nil {
 			if errors.Is(err, io.EOF) {
 				break
@@ -121,20 +191,121 @@ func Rotate(output io.Writer, image io.Reader, degrees int, ccw bool) error {
 		x := (width - 1) - (count / height)
 		y := count % height
 
-		matrix[y][x] = b
+		matrix[y][x] = string(b)
 
 		count++
 	}
 
-	// TODO
-	for _, y := range matrix {
-		for _, x := range y {
-			_, err := fmt.Fprint(output, string(x)+" ")
-			if err != nil {
-				return fmt.Errorf("could not write to output: %w", err)
+	buf := bufio.NewWriter(w)
+	defer buf.Flush()
+
+	lines := make([]string, len(matrix))
+	for i, v := range matrix {
+		lines[i] = strings.Join(v, " ")
+	}
+	_, err := buf.WriteString(strings.Join(lines, "\n"))
+	if err != nil {
+		return fmt.Errorf("could not write string: %w", err)
+	}
+
+	return nil
+}
+
+func rotate180(r io.Reader, w io.Writer, width, height int) error {
+	matrix := make([][]string, height)
+	for i := range matrix {
+		matrix[i] = make([]string, width)
+	}
+
+	rd := bufio.NewReader(r)
+	var count int
+	for {
+		b, err := rd.ReadByte()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
 			}
+			return fmt.Errorf("could not read byte: %w", err)
 		}
-		fmt.Fprint(output, "\n")
+
+		space := false
+		switch b {
+		case ' ', '\t', '\n', '\r':
+			space = true
+		}
+		if space {
+			continue
+		}
+
+		x := (width - 1) - count%width
+		y := (height - 1) - (count / width)
+
+		matrix[y][x] = string(b)
+
+		count++
+	}
+
+	buf := bufio.NewWriter(w)
+	defer buf.Flush()
+
+	lines := make([]string, len(matrix))
+	for i, v := range matrix {
+		lines[i] = strings.Join(v, " ")
+	}
+	_, err := buf.WriteString(strings.Join(lines, "\n"))
+	if err != nil {
+		return fmt.Errorf("could not write string: %w", err)
+	}
+
+	return nil
+}
+
+func rotate270(r io.Reader, w io.Writer, width, height int) error {
+	width, height = height, width
+
+	matrix := make([][]string, height)
+	for i := range matrix {
+		matrix[i] = make([]string, width)
+	}
+
+	rd := bufio.NewReader(r)
+	var count int
+	for {
+		b, err := rd.ReadByte()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return fmt.Errorf("could not read byte: %w", err)
+		}
+
+		space := false
+		switch b {
+		case ' ', '\t', '\n', '\r':
+			space = true
+		}
+		if space {
+			continue
+		}
+
+		x := count / height
+		y := (height - 1) - count%height
+
+		matrix[y][x] = string(b)
+
+		count++
+	}
+
+	buf := bufio.NewWriter(w)
+	defer buf.Flush()
+
+	lines := make([]string, len(matrix))
+	for i, v := range matrix {
+		lines[i] = strings.Join(v, " ")
+	}
+	_, err := buf.WriteString(strings.Join(lines, "\n"))
+	if err != nil {
+		return fmt.Errorf("could not write string: %w", err)
 	}
 
 	return nil
